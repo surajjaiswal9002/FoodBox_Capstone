818e7ea701bc0dbb2d5ba11c5e719c7c9b0f700f
Suraj Kumar Jaiswal <46620339+surajjaiswal9002@users.noreply.github.com>
2025-07-07 12:37:46 +0530

    Update GlobalExceptionHandler.java

--- Backend/src/main/java/com/api/foodbox/GlobalExceptionHandler.java
+++ Backend/src/main/java/com/api/foodbox/GlobalExceptionHandler.java
@@ -3,14 +3,10 @@ package com.api.foodbox;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
-
 @ControllerAdvice
-
 public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
-	public String handleException1(Exception e) 
-	{
+	public String handleException1(Exception e){
 		e.printStackTrace();
 		return " Error has occurred, please contact to Administrator";
 	}
